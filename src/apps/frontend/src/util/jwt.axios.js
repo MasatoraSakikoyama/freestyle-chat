fff8d78725f74f3d5725bba669c26ef88985926f
MasatoraSakikoyama <sakikoyama@icloud.com>
2017-04-02 11:05:17 +0000

    evalで共通化

--- src/apps/frontend/src/util/jwt.axios.js
+++ src/apps/frontend/src/util/jwt.axios.js
@@ -1,42 +1,35 @@
-const jwt = axios.create();
+const POST = 'post';
+const PUT = 'put';
+const DELETE = 'delete';
+const jwtAxios = axios.create();
 
-function _post(url, payload) {
+function _getTokenSetPromise(method, url, payload) {
+    if ([POST, PUT, DELETE].indexOf(method) === -1) {
+        throw 'Invalid method';
+    }
+    let evalTarget = `jwtAxios.${method}(url${payload ? ', payload)' : ')'}`;
     return new Promise((resolve, reject) => {
         axios.get('/api/session/token')
             .then(response => {
-                jwt.defaults.headers.common['Authorization'] = response.data.token;
-                resolve(jwt.post(url, payload));
+                jwtAxios.defaults.headers.common['Authorization'] = response.data.token;
+                resolve(eval(evalTarget));
             })
             .catch(error => {
                 reject(error);
             });
     });
+}
+
+function _post(url, payload) {
+    return _getTokenSetPromise(POST, url, payload);
 }
 
 function _put(url, payload) {
-    return new Promise((resolve, reject) => {
-        axios.get('/api/session/token')
-            .then(response => {
-                jwt.defaults.headers.common['Authorization'] = response.data.token;
-                resolve(jwt.put(url, payload));
-            })
-            .catch(error => {
-                reject(error);
-            });
-    });
+    return _getTokenSetPromise(PUT, url, payload);
 }
 
 function _delete(url) {
-    return new Promise((resolve, reject) => {
-        axios.get('/api/session/token')
-            .then(response => {
-                jwt.defaults.headers.common['Authorization'] = response.data.token;
-                resolve(jwt.delete(url));
-            })
-            .catch(error => {
-                reject(error);
-            });
-    });
+    return _getTokenSetPromise(DELETE, url);
 }
 
 export default {
