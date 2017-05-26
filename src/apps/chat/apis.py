f769f38aa2eca3315cce99cfcc11092b131d09a4
MasatoraSakikoyama <sakikoyama@icloud.com>
2017-05-27 00:24:15 +0900

    redisのキー検索修正

--- src/apps/chat/apis.py
+++ src/apps/chat/apis.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 from json import dumps
 
-from django.views.decorators.http import require_http_methods, condition
+from django.views.decorators.http import require_http_methods
 from django.http import HttpResponse
 from django.core.cache import caches
 
 from .utils import datetime_default
 
 
-@condition(etag_func=None)
 @require_http_methods(['GET'])
 def messages_api(request, room_id):
     # ToDo: StreamingHttpResponseでSSEにしたい
     if request.method == 'GET':
         cache = caches['messages']
-        keys = cache.keys('*{}_*'.format(room_id))
+        # redisのprefixを検索文字列に含めると上手く取れない
+        keys = cache.keys('{}_*'.format(room_id))
         messages = list(cache.get_many(keys).values())
         return HttpResponse(
             content=dumps(messages, default=datetime_default),
