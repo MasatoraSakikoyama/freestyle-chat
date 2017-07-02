e333b53186d2ba8de11293931cba74396b384c38
MasatoraSakikoyama <sakikoyama@icloud.com>
2017-07-03 02:05:04 +0900

    create asgi.py

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 
-import channels.asgi
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production")
-
-# application = get_wsgi_application()
-channel_layer = channels.asgi.get_channel_layer
+application = get_wsgi_application()
