ef037ac2e5cd58db879980e6bf0a6a62d9dff017
MasatoraSakikoyama <sakikoyama@icloud.com>
2017-07-03 23:59:43 +0900

    heroku debug true

--- src/settings/heroku.py
+++ src/settings/heroku.py
@@ -9,7 +9,7 @@ from .base import *
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "".join(random.choice(string.printable) for i in range(40)))
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [os.environ['DOMAIN']]
 
